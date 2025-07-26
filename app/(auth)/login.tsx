ecf7cd80ba8361c8eb20472b88518158adcb1313
thukralvinayak2003 <thukralvinayak2003@gmail.com>
2025-07-26 23:17:05 +0530

    Ui Update For Login

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,7 +1,6 @@
 import { login } from "@/features/auth/authSlice";
 import { AppDispatch, RootState } from "@/store";
 import { Ionicons } from "@expo/vector-icons";
-import { BlurView } from "expo-blur";
 import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
@@ -43,87 +42,85 @@ export default function LoginScreen() {
   return (
     <ImageBackground
       source={require("../../assets/images/background.png")}
-      className="flex-1 px-6"
-      resizeMode="cover"
+      style={{ flex: 1 }}
+      resizeMode="cover" // Ensures the image covers the whole area
+      className="px-6"
     >
       {/* Logo */}
       <View className="items-center">
         <Image
           source={require("../../assets/images/logo.png")}
-          style={{ width: 250, height: 250 }}
+          style={{ width: 220, height: 220 }}
           resizeMode="contain"
         />
       </View>
 
-      {/* Glassmorphic Form */}
+      {/* Glassmorphic Form
       <BlurView
         intensity={120}
         tint="dark"
         className="w-full max-w-md rounded-3xl border border-white/20 bg-black/60 p-6"
-      >
-        {/* Header */}
-        <Text className="text-white text-4xl font-bold text-center mb-2">
-          Welcome Back
-        </Text>
-        <Text className="text-slate-300 text-base text-center mb-8">
-          Sign in to continue
-        </Text>
+      > */}
+      {/* Header */}
+      <Text className="text-white text-4xl font-bold text-center mb-2">
+        Welcome Back
+      </Text>
+      <Text className="text-[#facc15] font-medium text-base text-center mb-8">
+        Sign in to continue
+      </Text>
 
-        {/* phoneNumber Input */}
-        <View className="mb-5">
-          <Text className="text-white text-sm mb-2 ml-1">Phone Number</Text>
-          <TextInput
-            placeholder="1234567890"
-            value={phoneNumber}
-            onChangeText={setPhoneNumber}
-            keyboardType="number-pad"
-            autoCapitalize="none"
-            placeholderTextColor="#cbd5e1"
-            className="bg-white/10 border border-white/20 rounded-xl px-4 py-3 text-white"
-          />
-        </View>
+      {/* phoneNumber Input */}
+      <View className="mb-5">
+        <TextInput
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          keyboardType="number-pad"
+          autoCapitalize="none"
+          placeholderTextColor="#cbd5e1"
+          className="border border-[#facc15]/20 rounded-xl px-4 py-3 text-white"
+        />
+      </View>
 
-        {/* Password Input */}
-        <View className="mb-6 relative">
-          <Text className="text-white text-sm mb-2 ml-1">Password</Text>
-          <TextInput
-            placeholder="••••••••"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry={!showPassword}
-            placeholderTextColor="#cbd5e1"
-            className="bg-white/10 border border-white/20 rounded-xl px-4 py-3 text-white pr-12"
-          />
-          <TouchableOpacity
-            className="absolute right-4 top-11"
-            onPress={() => setShowPassword(!showPassword)}
-          >
-            <Ionicons
-              name={showPassword ? "eye-off" : "eye"}
-              size={22}
-              color="#cbd5e1"
-            />
-          </TouchableOpacity>
-        </View>
-
-        {/* Login Button */}
+      {/* Password Input */}
+      <View className="mb-16 relative">
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={!showPassword}
+          placeholderTextColor="#cbd5e1"
+          className=" border border-[#facc15]/20 rounded-xl px-4 py-3 text-white pr-12"
+        />
         <TouchableOpacity
-          onPress={handleLogin}
-          activeOpacity={0.9}
-          className="rounded-xl overflow-hidden shadow-xl"
-        >
-          <LinearGradient
-            colors={["#facc15", "#eab308"]}
-            className="py-3 rounded-xl items-center"
-          >
-            {loading ? (
-              <ActivityIndicator size="small" color="black" />
-            ) : (
-              <Text className="text-black font-semibold text-base">Log In</Text>
-            )}
-          </LinearGradient>
+          className="absolute right-4 top-3"
+          onPress={() => setShowPassword(!showPassword)}        >
+          <Ionicons
+            name={showPassword ? "eye-off" : "eye"}
+            size={22}
+            color="#cbd5e1"
+          />
         </TouchableOpacity>
-      </BlurView>
+      </View>
+
+      {/* Login Button */}
+      <TouchableOpacity
+        onPress={handleLogin}
+        activeOpacity={0.9}
+        className="rounded-xl overflow-hidden shadow-xl"
+      >
+        <LinearGradient
+          colors={["#facc15", "#eab308"]}
+          className="py-3 rounded-xl items-center"
+        >
+          {loading ? (
+            <ActivityIndicator size="small" color="black" />
+          ) : (
+            <Text className="text-black font-bold text-base">Log In</Text>
+          )}
+        </LinearGradient>
+      </TouchableOpacity>
+      {/* </BlurView> */}
     </ImageBackground>
   );
 }
